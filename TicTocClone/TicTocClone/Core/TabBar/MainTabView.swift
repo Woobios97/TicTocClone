8707527df41b81b444bbdbeaa9bfaae3dbcd5710
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2024-03-15 00:03:46 +0900

    feat: MainTabView 구축

---
+++ TicTocClone/TicTocClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,68 @@
+//
+//  MainTabView.swift
+//  TicTocClone
+//
+//  Created by 김우섭 on 3/14/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab = 0
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Text("Feed")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Person")
+                    }
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+
+            Text("Upload Post")
+                .tabItem { Image(systemName: "plus") }
+
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
