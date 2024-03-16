14b05db6ded29e8385613daa0f72cd173f455791
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2024-03-16 17:22:20 +0900

    docs: 폴더추가

---
+++ TicTocClone/TicTocClone/Core/Search/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  TicTocClone
+//
+//  Created by 김우섭 on 3/16/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
