60c8fb92831450779c09af0d7139ca6414c99efa
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2024-03-16 17:27:27 +0900

    feat: ExploreView 추가

--- TicTocClone/TicTocClone/Core/Search/ExploreView.swift
+++ TicTocClone/TicTocClone/Core/Search/ExploreView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(0..<20) { user in
+                        UserCell()
+                    }
+                }
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.top)
+        }
     }
 }
 
